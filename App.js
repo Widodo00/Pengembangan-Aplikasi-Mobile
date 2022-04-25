1f85d729028a4bb6bea78160ccafe997df84aa37
Widodo00 <77629007+Widodo00@users.noreply.github.com>
2022-04-26 01:22:42 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,31 +1,21 @@
-import {Text, View, Pressable, ScrollView, Modal} from 'react-native';
-import {MaterialCommunityIcons, AntDesign, Feather, Entypo, FontAwesome5, Ionicons, FontAwesome } from '@expo/vector-icons';
-import styles from "./style";
-import data from "./data.json";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import home from './src/home/home';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.judul}>
-        <Text style={styles.teksjudul}>Kontak</Text>
-      </View>
-      <View style={styles.halaman}>
-      <ScrollView>
-      {data.map(post => {
-        return(
-          <View style={styles.pesanan}>
-            <>
-              <View style={styles.nama}>
-                <Text style={styles.teksnama}>{post.nama}</Text>
-              </View><View style={styles.no}>
-                <Text style={styles.teksno}>{post.no}</Text>
-              </View>
-            </>
-          </View>
-        )
-      })}
-      </ScrollView>
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+        headerShown: false
+      }}
+      >
+        <Stack.Screen name='home' component={home} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
+
+export default App;
